e719c7399fb5f5bc71b527e89a60896c1f6b074c
azharirahmaputri-123 <azharirahmaputri@gmail.com>
2026-03-26 13:35:46 +0700

    Update AquaFeedApp.java

--- src/AquaFeedApp.java
+++ src/AquaFeedApp.java
@@ -1,1 +1,12 @@
+public class AquaFeedApp {
+    public static void main(String[] args) {
+        System.out.println("--- AquaFeed Pro Operation ---");
+        
+        FishFeeder koiFeeder = new PelletFeeder("F-01", "Koi", 500, "Large");
+        FishFeeder fryFeeder = new LiquidFeeder("F-02", "Burayak", 100, 2.5);
 
+        koiFeeder.dispense();
+        System.out.println("-------------------------");
+        fryFeeder.dispense();
+    }
+}
